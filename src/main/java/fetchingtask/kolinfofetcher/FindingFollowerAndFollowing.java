cc7cd4dd0c30764cbe5069a7e5b5d571696130e9
Hepha4438 <trantrunghieudo@gmail.com>
2024-12-20 01:25:19 +0700

    Delete src/main/java directory

--- src/main/java/fetchingtask/kolinfofetcher/FindingFollowerAndFollowing.java
+++
@@ -1,9 +0,0 @@
-package fetchingtask.kolinfofetcher;
-
-import fetchingtask.Task;
-
-import java.util.Set;
-
-public interface FindingFollowerAndFollowing extends Task {
-    public Set<String> findingPerson(String userLink, String type);
-}
